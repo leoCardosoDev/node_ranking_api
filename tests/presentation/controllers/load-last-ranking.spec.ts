36447b2a7f2cd7762fbfef282deaf6b9ff18e7f7
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-24 11:03:05 -0700

    test: ensure LoadLastRankingController return 200 on success

--- tests/presentation/controllers/load-last-ranking.spec.ts
+++ tests/presentation/controllers/load-last-ranking.spec.ts
@@ -1,6 +1,7 @@
 import { RankingScore } from '@/domain/entities'
 import { LastRankingLoader } from '@/domain/usecases'
 import { LoadLastRankingController } from '@/presentation/controllers'
+import { RankingScoreViewModel } from '@/presentation/view-models'
 
 const mockRankingScore: RankingScore[] = [{
   player: {
@@ -25,6 +26,29 @@ const mockRankingScore: RankingScore[] = [{
   ]
 }]
 
+const mockRankingScoreViewModel: RankingScoreViewModel[] = [{
+  player: {
+    name: 'any_player_name',
+    country: 'any_player_country'
+  },
+  score: 1,
+  matchDate: '2020-01-10T03:00:00.000Z',
+  heroes: [
+    {
+      name: 'any_hero_name',
+      level: 1
+    },
+    {
+      name: 'other_hero_name',
+      level: 2
+    },
+    {
+      name: 'another_hero_name',
+      level: 3
+    }
+  ]
+}]
+
 
 class LastRankingLoaderStub implements LastRankingLoader {
   async load(): Promise<RankingScore[]> {
@@ -32,14 +56,37 @@ class LastRankingLoaderStub implements LastRankingLoader {
   }
 }
 
+type SutTypes = {
+  sut: LoadLastRankingController
+  lastRankingLoaderStub: LastRankingLoaderStub
+}
+
+const makeSut = (): SutTypes => {
+  const lastRankingLoaderStub = new LastRankingLoaderStub()
+  const sut = new LoadLastRankingController(lastRankingLoaderStub)
+
+  return {
+    sut,
+    lastRankingLoaderStub
+  }
+}
+
 describe('LoadLastRanking Controller', () => {
   it('should call LastRankingLoader with correct values', async () => {
-    const lastRankingLoaderStub = new LastRankingLoaderStub()
-    const sut = new LoadLastRankingController(lastRankingLoaderStub)
+    const { sut, lastRankingLoaderStub } = makeSut()
     const loadSpy = jest.spyOn(lastRankingLoaderStub, 'load')
     await sut.handle()
     expect(loadSpy).toHaveBeenCalledWith()
     expect(loadSpy).toHaveBeenCalledTimes(1)
+  })
+
+  it('should returns an RankingViewModel on success', async () => {
+    const { sut } = makeSut()
+    const response = await sut.handle()
+    expect(response).toEqual({
+      statusCode: 200,
+      data: mockRankingScoreViewModel
+    })
   })
 })
 
