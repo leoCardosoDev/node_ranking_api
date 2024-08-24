1e2ecfcb68128d2f772810d4b72aaa20294d82f4
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-24 12:31:44 -0700

    refactor: add intelligence to ViewModel

--- src/presentation/controllers/load-last-ranking.ts
+++ src/presentation/controllers/load-last-ranking.ts
@@ -7,12 +7,7 @@ export class LoadLastRankingController implements Controller {
   async handle(): Promise<HttpResponse<RankingScoreViewModel[] | Error>> {
     try {
       const ranking = await this._lastRankingLoader.load()
-      return ok(
-        ranking.map(item => ({
-          ...item,
-          matchDate: item.matchDate.toISOString()
-        }))
-      )
+      return ok(RankingScoreViewModel.mapCollection(ranking))
     } catch (error) {
       return serverError(error as Error)
     }
