0c0fe7de2cdfebafe4b9bf18f02b797db2e7eaeb
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-24 11:56:26 -0700

    feat: ensure LoadLastRankingController return 500 if LastRankingLoader throws

--- src/presentation/controllers/load-last-ranking.ts
+++ src/presentation/controllers/load-last-ranking.ts
@@ -4,14 +4,21 @@ import { RankingScoreViewModel } from '../view-models'
 
 export class LoadLastRankingController implements Controller {
   constructor(private readonly _lastRankingLoader: LastRankingLoader) {}
-  async handle(): Promise<HttpResponse<RankingScoreViewModel[]>> {
-    const ranking = await this._lastRankingLoader.load()
-    return {
-      statusCode: 200,
-      data: ranking.map(item => ({
-        ...item,
-        matchDate: item.matchDate.toISOString()
-      }))
+  async handle(): Promise<HttpResponse<RankingScoreViewModel[] | Error>> {
+    try {
+      const ranking = await this._lastRankingLoader.load()
+      return {
+        statusCode: 200,
+        data: ranking.map(item => ({
+          ...item,
+          matchDate: item.matchDate.toISOString()
+        }))
+      }
+    } catch (error) {
+      return {
+        statusCode: 500,
+        data: error as Error
+      }
     }
   }
 }
